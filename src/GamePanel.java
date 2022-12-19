dd7ce198a8843f5ac0b0faed63e4c84d48377fef
Martin Parunev <parunev@gmail.com>
2022-12-19 13:56:03 +0200

    The Mechanism

---
+++ src/GamePanel.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+
+//works as game screen
+public class GamePanel extends JPanel implements Runnable {
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 default size of the player character, NPCs and map tiles
+    final int scale = 3;
+    final int tileSize = originalTileSize * scale; // 16x3 (48x48) pixels on the screen. Pretty common for retro games.
+
+    // 16 tiles horizontally, 12 tiles vertically 4:3 ratio
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    //it keeps your program running until you stop it
+    Thread gameThread;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight)); // set the size of this class (JPanel)
+        this.setBackground(Color.black);
+
+        // if set true, all the drawings from this component will be done in an offscreen painting buffer
+        // enabling this can improve game's rendering performance
+        this.setDoubleBuffered(true);
+    }
+
+    // passing this(GamePanel) to this thread constructor, instantiate
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start(); // automatically calls the run method
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Blueprint Adventure Game");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        // causes this window to be sized to fit the preferred size
+        // and layouts of its subcomponents (=GamePanel)
+        window.pack();
+
+        //not specify the location of the window, the window will be displayed at the center of the screen
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+}
