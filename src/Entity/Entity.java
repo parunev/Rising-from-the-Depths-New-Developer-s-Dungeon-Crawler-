066f4fb9599526bbbe90fabba1224254dd72c4a1
Martin Parunev <parunev@gmail.com>
2023-01-02 17:36:51 +0200

    commit

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -240,7 +240,7 @@ public class Entity {
                 dyingAnimation(g2);
             }
 
-            g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+            g2.drawImage(image, screenX, screenY, null);
             changeAlpha(g2, 1F);
         }
     }
