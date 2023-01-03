c742905aca9379e84bc3d4b99899c37209988092
Martin Parunev <parunev@gmail.com>
2023-01-03 14:33:25 +0200

    Changed the resolution to 960 x 576. Looks better.

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -20,9 +20,9 @@ public class GamePanel extends JPanel implements Runnable {
     public final int tileSize = originalTileSize * scale; // 16x3 (48x48) pixels on the screen. Pretty common for retro games.
 
     // 16 tiles horizontally, 12 tiles vertically 4:3 ratio
-    public final int maxScreenCol = 16;
+    public final int maxScreenCol = 20;
     public final int maxScreenRow = 12;
-    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    public final int screenWidth = tileSize * maxScreenCol; // 960 pixels
     public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
     // WORLD SETTINGS
